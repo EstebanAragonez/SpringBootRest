48858994b8629f757913265918546664ba2a8741
Esteban Aragonez <aragonezleo@gmail.com>
2024-11-26 17:43:46 -0500

    se  termina de crear los controladores y se crea el ProductDTO

--- src/main/java/com/application/rest/SpringBootRest/controllers/MakerController.java
+++ src/main/java/com/application/rest/SpringBootRest/controllers/MakerController.java
@@ -3,21 +3,21 @@ package com.application.rest.SpringBootRest.controllers;
 import com.application.rest.SpringBootRest.controllers.dto.MakerDTO;
 import com.application.rest.SpringBootRest.entities.Maker;
 import com.application.rest.SpringBootRest.service.IMakerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/maker")
+@RequiredArgsConstructor
 public class MakerController {
 
-    @Autowired
-    private IMakerService makerService;
+
+    private final IMakerService makerService;
 
     @GetMapping("/find/{id}")
     public ResponseEntity<?> findByid(@PathVariable Long id){
@@ -35,5 +35,53 @@ public class MakerController {
             return ResponseEntity.ok(makerDTO);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<?>findAll(){
+        List<MakerDTO> makerList = makerService.findAll()
+                .stream()
+                .map(maker -> MakerDTO.builder()
+                        .id(maker.getId())
+                        .name(maker.getName())
+                        .productList(maker.getProductList())
+                        .build())
+                .toList();
+        return ResponseEntity.ok(makerList);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save(@RequestBody MakerDTO makerDTO) throws URISyntaxException {
+
+        if(makerDTO.getName().isBlank()){
+            return ResponseEntity.badRequest().build();
+        }
+        makerService.save(Maker.builder()
+                .name(makerDTO.getName())
+                .build());
+        return ResponseEntity.created(new URI("/api/maker/save")).build();
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?>updateMaker(@PathVariable Long id, @RequestBody MakerDTO makerDTO){
+        Optional<Maker> makerOptional = makerService.findById(id);
+
+        if(makerOptional.isPresent()){
+            Maker maker = makerOptional.get();
+            maker.setName(makerDTO.getName());
+            makerService.save(maker);
+            return ResponseEntity.ok("Registro Actualizado");
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+
+        if (id != null) {
+            makerService.deleteById(id);
+            return ResponseEntity.ok("Registro eliminado");
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
---
+++ src/main/java/com/application/rest/SpringBootRest/controllers/dto/ProductDTO.java
@@ -0,0 +1,23 @@
+package com.application.rest.SpringBootRest.controllers.dto;
+
+import com.application.rest.SpringBootRest.entities.Maker;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private Maker maker;
+}
